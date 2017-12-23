7946029da1cc7bd8f31305d2b3344fbe5a16b794
Yuriy Sirotenko <spider4216@gmail.com>
2017-12-23 14:30:03 +0600

    sign up action

--- src/com/epam/action/SignUpMainAction.java
+++ src/com/epam/action/SignUpMainAction.java
@@ -1,11 +1,16 @@
 package com.epam.action;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class SignUpMainAction implements IAction {
-	public void execute(HttpServletRequest request, HttpServletResponse response) {
+import com.epam.component.view.Viewer;
 
+public class SignUpMainAction implements IAction {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Viewer.execute(request, response, "signUp.jsp");
 	}
 
 }
