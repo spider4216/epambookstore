ff617432eb083d6ae9748b12750ea084cada34b9
Yuriy Sirotenko <spider4216@gmail.com>
2017-12-28 22:40:53 +0600

    access for sign in and up

--- src/com/epam/action/SignInMainAction.java
+++ src/com/epam/action/SignInMainAction.java
@@ -1,16 +1,24 @@
 package com.epam.action;
 
 import java.io.IOException;
+import java.security.AccessControlException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.epam.component.service_locator.ServiceLocator;
+import com.epam.component.service_locator.ServiceLocatorException;
 import com.epam.component.view.Viewer;
 
 public class SignInMainAction implements IAction {
 
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ServiceLocatorException {
+		// TODO constant instead string (service name)
+		if (ServiceLocator.getInstance().getService("user") != null) {
+			throw new AccessControlException("You have already signed in");
+		}
+		
 		Viewer.execute(request, response, "signIn.jsp");
 	}
 	
--- src/com/epam/action/SignUpMainAction.java
+++ src/com/epam/action/SignUpMainAction.java
@@ -1,15 +1,22 @@
 package com.epam.action;
 
 import java.io.IOException;
+import java.security.AccessControlException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.epam.component.service_locator.ServiceLocator;
+import com.epam.component.service_locator.ServiceLocatorException;
 import com.epam.component.view.Viewer;
 
 public class SignUpMainAction implements IAction {
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ServiceLocatorException {
+		if (ServiceLocator.getInstance().getService("user") != null) {
+			throw new AccessControlException("You have already signed up");
+		}
+		
 		Viewer.execute(request, response, "signUp.jsp");
 	}
 
