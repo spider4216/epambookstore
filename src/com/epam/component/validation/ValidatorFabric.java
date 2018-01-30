64f382decd5187d02e35b32a18e1fd7ae6d4f9de
Yuriy Sirotenko <spider4216@gmail.com>
2018-01-30 21:36:13 +0600

    default in switch was added

--- src/com/epam/component/validation/ValidatorFabric.java
+++ src/com/epam/component/validation/ValidatorFabric.java
@@ -1,6 +1,11 @@
 package com.epam.component.validation;
 
+import com.epam.component.lang.Lang;
+import com.epam.component.service_locator.ServiceLocator;
+import com.epam.component.service_locator.ServiceLocatorEnum;
+import com.epam.component.service_locator.ServiceLocatorException;
 import com.epam.component.validation.exception.ValidationException;
+import com.epam.component.validation.exception.ValidatorNotFound;
 import com.epam.component.validation.validator.PasswordValidator;
 import com.epam.component.validation.validator.RequiredValidator;
 import com.epam.component.validation.validator.UniqueUsernameValidator;
@@ -14,7 +19,8 @@ import com.epam.component.validation.validator.UsernameValidator;
 public abstract class ValidatorFabric {
 	public abstract Boolean execute(String value) throws ValidationException;
 	
-	public static ValidatorFabric initial(ValidatorEnum name) {
+	public static ValidatorFabric initial(ValidatorEnum name) throws ValidatorNotFound, ServiceLocatorException {
+		Lang lang = (Lang) ServiceLocator.getInstance().getService(ServiceLocatorEnum.LANG);
 		ValidatorFabric validator = null;
 		
 		switch (name) {
@@ -30,6 +36,8 @@ public abstract class ValidatorFabric {
 			case REQUIRED :
 				validator = new RequiredValidator();
 				break;
+			default:
+				throw new ValidatorNotFound(lang.getValue("validator_not_found"));
 		}
 		
 		return validator;
---
+++ src/com/epam/component/validation/exception/ValidatorNotFound.java
@@ -0,0 +1,7 @@
+package com.epam.component.validation.exception;
+
+public class ValidatorNotFound extends Exception {
+	public ValidatorNotFound(String msg) {
+		super(msg);
+	}
+}
